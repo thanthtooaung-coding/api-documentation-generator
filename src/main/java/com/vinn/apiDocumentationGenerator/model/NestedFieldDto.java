1a764dbf1d35b3721c04e77b070e1927ea1f1e48
Thant Htoo Aung <thanthtoo1285@gmail.com>
2025-01-18 23:31:00 +0630

    [update] for Object type parameter

--- src/main/java/com/vinn/apiDocumentationGenerator/model/NestedFieldDto.java
+++ src/main/java/com/vinn/apiDocumentationGenerator/model/NestedFieldDto.java
@@ -5,16 +5,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class ParameterDto {
-    private String name;
-    private String type;
-    private boolean required;
-    private String description;
-    private List<NestedFieldDto> nestedFields;
+public class NestedFieldDto {
+    private String fieldName;
+    private String fieldType;
 }
